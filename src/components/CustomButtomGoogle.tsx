5fdcebc73395a896edf1178be965824823102eaa
Tupis <joaoh.tupinamba@gmail.com>
2022-10-01 16:36:01 -0300

    feat: create button login google

---
+++ src/components/CustomButtomGoogle.tsx
@@ -0,0 +1,14 @@
+import { useGoogleLogin } from "@react-oauth/google";
+import { LoginGoogle } from "../services/GoogleServices";
+
+const CustomButtom = (props: any) => {
+  const { setToken } = props;
+
+  const login = useGoogleLogin({
+    onSuccess: async (response) => setToken(await LoginGoogle(response)),
+  });
+
+  return <button onClick={() => login()}>Logar com o Google</button>;
+};
+
+export default CustomButtom;
