71f634791683016097ddb134db3c67a4808afcd6
Tupis <joaoh.tupinamba@gmail.com>
2022-10-06 00:44:11 -0300

    fix: right types, feat: create base url to api

--- src/services/apis.ts
+++ src/services/apis.ts
@@ -1,7 +1,11 @@
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
 
 const baseURLGoogle = "https://www.googleapis.com/oauth2/v3/userinfo";
 
-export const APIGoogle = axios.create({
+export const APIGoogle: AxiosInstance = axios.create({
   baseURL: baseURLGoogle,
+});
+
+export const APIUsers: AxiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_CONNECTION,
 });
