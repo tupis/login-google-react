67a824d9c8354c6f7020c193399e5ce1e51be0cf
Tupis <joaoh.tupinamba@gmail.com>
2022-10-14 10:25:13 -0300

    feat: test log

--- src/services/UsersServices.ts
+++ src/services/UsersServices.ts
@@ -3,14 +3,18 @@ import { APIUsers } from "./apis";
 export const login: (params: any) => Promise<void> = async (
   params
 ): Promise<void> => {
-  const response = await APIUsers.post("/login", params);
-  localStorage.setItem("user", JSON.stringify(response.data.user));
-  localStorage.setItem("token", JSON.stringify(response.data.token));
+  const response = await APIUsers.post("/login", params).then((response) =>
+    console.log(response)
+  );
+  // localStorage.setItem("user", JSON.stringify(response.data.user));
+  // localStorage.setItem("token", JSON.stringify(response.data.token));
 };
 
 export const register: (params: any) => Promise<void> = async (
   params
 ): Promise<void> => {
-  const response = await APIUsers.post("/login", params);
-  localStorage.setItem("token", JSON.stringify(response.data.token));
+  const response = await APIUsers.post("/register", params).then((response) =>
+    console.log(response.data)
+  );
+  // localStorage.setItem("token", JSON.stringify(response.data.token));
 };
